ac47e5718f26280c3bde8304b7fc438de27df3c5
dhlim55 <dla0889@naver.com>
2025-08-01 01:43:08 +0900

    Redirect page 구현

---
+++ src/SurveyRedirect.js
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+const SurveyRedirect = () => {
+  const links = [
+    "https://dhlim55.github.io/sdfchat1/",
+    "https://dhlim55.github.io/sdfchatc2/",
+    "https://dhlim55.github.io/sdfchat/"
+  ]
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * links.length);
+    const selectedLink = links[randomIndex];
+    window.location.href = selectedLink;
+  }, []);
+
+  return <p>설문 페이지로 이동 중입니다...</p>;
+};
+
+export default SurveyRedirect;
